826bacdf355e370bbbd1677a7c624f73edd27abc
Adrian Kuendig <adriankue@gmail.com>
2012-06-22 21:44:35 +0200

    only send values if different than default
    not tested

--- server/cache.go
+++ server/cache.go
@@ -21,9 +21,9 @@ func init() {
 }
 
 type cachedLocation struct {
-	X, Y    int
-	Query   string
-	Tpe     string
+	X, Y    int    "/c"
+	Query   string "/c"
+	Tpe     string "/c"
 	Json    string
 	Expires time.Time
 }
